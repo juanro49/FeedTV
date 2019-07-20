699d0fef966ed8dd0dfaea932525e45d139a9e71
juanro49 <juanrobertogarciasanchez@gmail.com>
2019-07-20 19:21:38 +0200

    Reemplazar Volleyplus por Volley y arreglar fallo al cargar lista de canales en Android 9

--- app/src/main/java/org/juanro/feedtv/JSONParser/JSONParser.java
+++ app/src/main/java/org/juanro/feedtv/JSONParser/JSONParser.java
@@ -24,8 +24,8 @@ import android.util.Log;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
-import com.android.volley.error.VolleyError;
-import com.android.volley.request.JsonObjectRequest;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -112,7 +112,7 @@ public class JSONParser
 					@Override
 					public void onResponse(JSONObject response)
 					{
-						Log.i(TAG, "Response OK");
+						Log.i(TAG, "JSON obtenido correctamente");
 
 						// Parsear elementos del JSON
 						try
